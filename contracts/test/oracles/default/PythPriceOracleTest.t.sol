60f6b9b81288245b9cb3db04f7bfb63cfd8403a7
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-08-04 17:55:01 +0200

    zkevm config and oracle tests

--- contracts/test/oracles/default/PythPriceOracleTest.t.sol
+++ contracts/test/oracles/default/PythPriceOracleTest.t.sol
@@ -6,8 +6,10 @@ import { MasterPriceOracle } from "../../../oracles/MasterPriceOracle.sol";
 import { BaseTest } from "../../config/BaseTest.t.sol";
 import { IPyth } from "@pythnetwork/pyth-sdk-solidity/MockPyth.sol";
 import { PythStructs } from "@pythnetwork/pyth-sdk-solidity/PythStructs.sol";
+import { ERC20Upgradeable } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+import { BasePriceOracle } from "../../../oracles/BasePriceOracle.sol";
 
-contract PythOraclesTest is BaseTest {
+contract PythPriceOracleTest is BaseTest {
   PythPriceOracle oracle;
   IPyth pythOracle;
   MasterPriceOracle mpo;
@@ -19,6 +21,7 @@ contract PythOraclesTest is BaseTest {
   address neonPyth = 0x7f2dB085eFC3560AFF33865dD727225d91B4f9A5;
   address lineaPyth = 0xA2aa501b19aff244D90cc15a4Cf739D2725B5729;
   address polygonPyth = 0xff1a0f4744e8582DF1aE09D5611b887B6a12925C;
+  address zkevmPyth = 0xC5E56d6b40F3e3B5fbfa266bCd35C37426537c65;
 
   bytes32 ethUsdTokenPriceFeed = 0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace;
   bytes32 btcUsdTokenPriceFeed = 0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43;
@@ -30,10 +33,7 @@ contract PythOraclesTest is BaseTest {
     stable = ap.getAddress("stableToken");
     wtoken = ap.getAddress("wtoken");
     wbtc = ap.getAddress("wBTCToken");
-
-    mpo = MasterPriceOracle(ap.getAddress("MasterPriceOracle"));
     oracle = new PythPriceOracle();
-    vm.startPrank(mpo.admin());
 
     // create an array of bytes to pass to the oracle
     bytes32[] memory feedIds = new bytes32[](2);
@@ -46,6 +46,8 @@ contract PythOraclesTest is BaseTest {
       oracle.initialize(lineaPyth, ethUsdTokenPriceFeed, stable);
     } else if (block.chainid == POLYGON_MAINNET) {
       oracle.initialize(polygonPyth, maticUsdTokenPriceFeed, stable);
+    } else if (block.chainid == ZKEVM_MAINNET) {
+      oracle.initialize(zkevmPyth, ethUsdTokenPriceFeed, stable);
     } else {
       revert("Unsupported chain");
     }
@@ -88,5 +90,16 @@ contract PythOraclesTest is BaseTest {
     uint256 price = oracle.price(wbtc);
     uint256 priceMpo = mpo.price(wbtc);
     assertApproxEqRel(price, priceMpo, 1e16);
+  }
+
+  function testZkEvmTokenPrice() public fork(ZKEVM_MAINNET) {
+    PythStructs.Price memory pythPrice = IPyth(zkevmPyth).getPriceUnsafe(btcUsdTokenPriceFeed);
+    emit log_named_uint("price", uint256(uint64(pythPrice.price)));
+    emit log_named_uint("updated", pythPrice.publishTime);
+    emit log_named_uint("ts", block.timestamp);
+
+    uint256 price = oracle.price(wbtc);
+    uint256 priceMpo = mpo.price(wbtc);
+    assertApproxEqRel(price, priceMpo, 1e14);
   }
 }
