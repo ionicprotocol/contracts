82dcbf57d2c4664b0568012b7e96f18e53216305
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2023-08-15 10:04:41 +0300

    prettier

--- contracts/test/helpers/WithPool.sol
+++ contracts/test/helpers/WithPool.sol
@@ -60,7 +60,10 @@ contract WithPool is BaseTest {
       {
         FeeDistributor newImpl = new FeeDistributor();
         TransparentUpgradeableProxy proxy = TransparentUpgradeableProxy(payable(address(ionicAdmin)));
-        bytes32 bytesAtSlot = vm.load(address(proxy), 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103);
+        bytes32 bytesAtSlot = vm.load(
+          address(proxy),
+          0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103
+        );
         address admin = address(uint160(uint256(bytesAtSlot)));
         vm.prank(admin);
         proxy.upgradeTo(address(newImpl));
