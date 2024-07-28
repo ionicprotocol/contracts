a0a70db73564138db1b4d97875b667f954d808da
Rahul Sethuram <rahul.eth@icloud.com>
2024-07-28 19:47:26 +0400

    feat: deploy aero oracle

--- chainDeploy/mainnets/base.ts
+++ chainDeploy/mainnets/base.ts
@@ -1,64 +1,65 @@
-// import { ChainDeployConfig, deployChainlinkOracle } from "../helpers";
-// import { ChainlinkAsset } from "../../chains/types";
+import { ChainDeployConfig } from "../helpers";
+import { OracleTypes, SupportedAsset } from "../../chains/types";
+import { base } from "../../chains";
+import { deployAerodromeOracle } from "../helpers/oracles/aerodrome";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-// const assets = base.assets;
+const assets = base.assets;
 
-// export const deployConfig: ChainDeployConfig = {
-//   blocksPerYear: Number(base.specificParams.blocksPerYear),
-//   cgId: base.specificParams.cgId,
-//   nativeTokenName: "Base",
-//   nativeTokenSymbol: "ETH",
-//   stableToken: base.chainAddresses.STABLE_TOKEN,
-//   uniswap: {
-//     flashSwapFee: 30, // TODO set the correct fee
-//     hardcoded: [],
-//     uniswapData: [],
-//     uniswapOracleInitialDeployTokens: [],
-//     uniswapV2FactoryAddress: "0x8909Dc15e40173Ff4699343b6eB8132c65e18eC6",
-//     uniswapV2RouterAddress: "0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24",
-//     uniswapV3SwapRouter: "0x2626664c2603336E57B271c5C0b26F421741e481",
-//     uniswapV3Quoter: "0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a"
-//   },
-//   wtoken: base.chainAddresses.W_TOKEN,
-//   nativeTokenUsdChainlinkFeed: base.chainAddresses.W_TOKEN_USD_CHAINLINK_PRICE_FEED
-// };
+const pricesContract = "0xee717411f6E44F9feE011835C8E6FAaC5dEfF166";
 
-// const chainlinkAssets: ChainlinkAsset[] = base.assets
-//   .filter((asset) => asset.oracle === OracleTypes.ChainlinkPriceOracleV2)
-//   .map((asset) => ({
-//     aggregator: (asset.oracleSpecificParams as ChainlinkSpecificParams).aggregator,
-//     feedBaseCurrency: (asset.oracleSpecificParams as ChainlinkSpecificParams).feedBaseCurrency,
-//     symbol: asset.symbol
-//   }));
+export const deployConfig: ChainDeployConfig = {
+  blocksPerYear: Number(base.specificParams.blocksPerYear),
+  cgId: base.specificParams.cgId,
+  nativeTokenName: "Base",
+  nativeTokenSymbol: "ETH",
+  stableToken: base.chainAddresses.STABLE_TOKEN,
+  uniswap: {
+    flashSwapFee: 30, // TODO set the correct fee
+    hardcoded: [],
+    uniswapData: [],
+    uniswapOracleInitialDeployTokens: [],
+    uniswapV2FactoryAddress: "0x8909Dc15e40173Ff4699343b6eB8132c65e18eC6",
+    uniswapV2RouterAddress: "0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24",
+    uniswapV3SwapRouter: "0x2626664c2603336E57B271c5C0b26F421741e481",
+    uniswapV3Quoter: "0x3d4e44Eb1374240CE5F1B871ab261CD16335B76a"
+  },
+  wtoken: base.chainAddresses.W_TOKEN,
+  nativeTokenUsdChainlinkFeed: base.chainAddresses.W_TOKEN_USD_CHAINLINK_PRICE_FEED
+};
 
-// export const deploy = async ({ run, ethers, getNamedAccounts, deployments }): Promise<void> => {
-//   const { deployer } = await getNamedAccounts();
+const aerodromeAssets: SupportedAsset[] = base.assets.filter(
+  (asset) => asset.oracle === OracleTypes.AerodromePriceOracle
+);
 
-//   //// ChainLinkV2 Oracle
-//   await deployChainlinkOracle({
-//     run,
-//     ethers,
-//     getNamedAccounts,
-//     deployments,
-//     deployConfig,
-//     assets,
-//     chainlinkAssets
-//   });
+export const deploy = async ({ run, viem, getNamedAccounts, deployments }: HardhatRuntimeEnvironment): Promise<void> => {
+  const { deployer } = await getNamedAccounts();
 
-//   //// Uniswap V3 Liquidator Funder
-//   const uniswapV3LiquidatorFunder = await deployments.deploy("UniswapV3LiquidatorFunder", {
-//     from: deployer,
-//     args: [],
-//     log: true,
-//     waitConfirmations: 1
-//   });
-//   console.log("UniswapV3LiquidatorFunder: ", uniswapV3LiquidatorFunder.address);
+  //// ChainLinkV2 Oracle
+  await deployAerodromeOracle({
+    run,
+    viem,
+    getNamedAccounts,
+    deployments,
+    deployConfig,
+    assets: aerodromeAssets,
+    pricesContract
+  });
 
-//   const solidlySwapLiquidator = await deployments.deploy("SolidlySwapLiquidator", {
-//     from: deployer,
-//     args: [],
-//     log: true,
-//     waitConfirmations: 1
-//   });
-//   console.log("solidlySwapLiquidator: ", solidlySwapLiquidator.address);
-// };
+  //// Uniswap V3 Liquidator Funder
+  const uniswapV3LiquidatorFunder = await deployments.deploy("UniswapV3LiquidatorFunder", {
+    from: deployer,
+    args: [],
+    log: true,
+    waitConfirmations: 1
+  });
+  console.log("UniswapV3LiquidatorFunder: ", uniswapV3LiquidatorFunder.address);
+
+  const solidlySwapLiquidator = await deployments.deploy("SolidlySwapLiquidator", {
+    from: deployer,
+    args: [],
+    log: true,
+    waitConfirmations: 1
+  });
+  console.log("solidlySwapLiquidator: ", solidlySwapLiquidator.address);
+};
--- chains/base/assets.ts
+++ chains/base/assets.ts
@@ -13,6 +13,7 @@ export const WBTC = "0x1ceA84203673764244E05693e42E6Ace62bE9BA5";
 export const weETH = "0x04C0599Ae5A44757c0af6F9eC3b93da8976c150A";
 export const eUSD = "0xcfa3ef56d303ae4faaba0592388f19d7c3399fb4";
 export const bsdETH = "0xcb327b99ff831bf8223cced12b1338ff3aa322ff";
+export const ION = "0x3eE5e23eEE121094f1cFc0Ccc79d6C809Ebd22e5";
 
 export const assets: SupportedAsset[] = [
   {
@@ -138,6 +139,13 @@ export const assets: SupportedAsset[] = [
     initialSupplyCap: parseEther(String(6_500)).toString(),
     initialBorrowCap: parseEther(String(5_200)).toString(),
     initialCf: "70"
+  },
+  {
+    symbol: assetSymbols.ION,
+    underlying: ION,
+    name: "Ionic",
+    decimals: 18,
+    oracle: OracleTypes.AerodromePriceOracle
   }
 ];
 
