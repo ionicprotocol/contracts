8cd07c27d1425bb04949bbf5e2e8c7bf1bd28c8e
Rahul Sethuram <rahul.eth@icloud.com>
2024-07-31 16:30:09 +0400

    fix: update script

--- tasks/chain-specific/base.ts
+++ tasks/chain-specific/base.ts
@@ -13,11 +13,10 @@ task("market:base:rsr-ion-rewards", "Sets caps on a market").setAction(
     const ioneUSD = "0x9c2a4f9c5471fd36be3bbd8437a33935107215a1";
     const eUSD = "0xCfA3Ef56d303AE4fAabA0592388F19d7C3399FB4";
     const IONIC = "0x3eE5e23eEE121094f1cFc0Ccc79d6C809Ebd22e5";
-    const RSR = "0xab36452dbac151be02b16ca17d8919826072f64a"
+    const RSR = "0xab36452dbac151be02b16ca17d8919826072f64a";
     const pool = "0x05c9C6417F246600f8f5f49fcA9Ee991bfF73D13";
     const comptroller = "0x05c9C6417F246600f8f5f49fcA9Ee991bfF73D13";
     const markets = `${ionbsdETH},${ioneUSD}`;
-
 
     /*
     const comptrollerContract = await viem.getContractAt("IonicComptroller", comptroller as Address);
@@ -25,16 +24,15 @@ task("market:base:rsr-ion-rewards", "Sets caps on a market").setAction(
     console.log("tx: ", tx);
     await publicClient.waitForTransactionReceipt({ hash: tx });
     */
-    /*
-    const flywheelContract = await viem.getContractAt(
-      "IonicFlywheelBorrow",
-      (await deployments.get("IonicFlywheelBorrow_Borrow_ION")).address as Address
-    );
-    const rewardsContract = (await deployments.get("IonicFlywheelDynamicRewards_Borrow_ION")).address as Address;
+    // const flywheelContract = await viem.getContractAt(
+    //   "IonicFlywheelBorrow",
+    //   (await deployments.get("IonicFlywheelBorrow_Borrow_ION")).address as Address
+    // );
+    // const rewardsContract = (await deployments.get("IonicFlywheelDynamicRewards_Borrow_ION")).address as Address;
 
-    const tx = await flywheelContract.write.setFlywheelRewards([rewardsContract as Address]);
-    await publicClient.waitForTransactionReceipt({ hash: tx });
-    
+    // const tx = await flywheelContract.write.setFlywheelRewards([rewardsContract as Address]);
+    // await publicClient.waitForTransactionReceipt({ hash: tx });
+
     // STEP 1: upgrade markets to the new implementation
     console.log(`Upgrading market: ${ionbsdETH} to CErc20RewardsDelegate`);
     await run("market:upgrade", {
@@ -52,8 +50,7 @@ task("market:base:rsr-ion-rewards", "Sets caps on a market").setAction(
       signer: deployer
     });
     console.log("Market upgraded");
-    */
-    // STEP 2: send reward tokens to strategies 
+    // STEP 2: send reward tokens to strategies
     const rsrToken = await viem.getContractAt("EIP20Interface", RSR);
     const balance = await rsrToken.read.balanceOf([ionbsdETH]);
     if (balance < parseEther("170484.026388")) {
@@ -64,25 +61,24 @@ task("market:base:rsr-ion-rewards", "Sets caps on a market").setAction(
       await rsrToken.write.transfer([ioneUSD, parseEther("185308.724335")]);
     }
 
-    
     // NOTE: change name and reward token
-    await run("flywheel:deploy-dynamic-rewards-fw", { 
+    await run("flywheel:deploy-dynamic-rewards-fw", {
       name: "RSR",
       rewardToken: RSR,
       booster: "",
       strategies: markets,
-      pool 
+      pool
     });
 
     const flywheel = await viem.getContractAt(
       "IonicFlywheel",
       (await deployments.get("IonicFlywheel_RSR")).address as Address
     );
-    await run("approve-market-flywheel", { 
+    await run("approve-market-flywheel", {
       fwAddress: flywheel.address,
-      markets: markets 
+      markets: markets
     });
-    
+
     const tx = await flywheel.write.updateFeeSettings([0n, deployer as Address]);
     await publicClient.waitForTransactionReceipt({ hash: tx });
 
