e47437462ab2caa699c33b6cda4c69291d0eda19
Rahul Sethuram <rahul.eth@icloud.com>
2024-08-01 14:03:01 +0400

    fix: base

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -52,7 +52,7 @@ const config: HardhatUserConfig = {
       accounts: [process.env.DEPLOYER!]
     },
     base: {
-      url: process.env.OVERRIDE_RPC_URL ?? "https://base.llamarpc.com",
+      url: process.env.OVERRIDE_RPC_URL ?? "https://base.meowrpc.com",
       accounts: [process.env.DEPLOYER!],
       verify: {
         etherscan: {
