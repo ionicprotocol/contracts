e4e7eae0fbe71a9058fbb151bca1868fd3963937
Rahul Sethuram <rahul.eth@icloud.com>
2024-08-19 10:02:54 +0400

    fix: deploy flywheels

--- tasks/chain-specific/base/index.ts
+++ tasks/chain-specific/base/index.ts
@@ -2,5 +2,6 @@ import "./markets";
 import "./single";
 import "./rewards";
 import "./tokens";
+import "./disableFlywheel";
 
 export const COMPTROLLER = "0x05c9C6417F246600f8f5f49fcA9Ee991bfF73D13";
